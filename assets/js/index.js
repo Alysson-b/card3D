1aeb8b1dcb3f0ea997c88f2296f462671774dbd9
Alisson Barbosa <161394750+Alysson-b@users.noreply.github.com>
2024-07-25 13:34:27 -0300

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+const cards = document.querySelectorAll('.card')
+
+cards.forEach((card) => {
+    card.addEventListener('mousemove', (e) =>{
+       if(card.classList.contains('active')){ 
+        const rect = card.getBoundingClientRect();
+    
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+    
+        const centerX = rect.width / 2;
+        const centerY = rect.height / 2;
+    
+        const rotateX = (y - centerY) / 10;
+        const rotateY = (x - centerX) / 10;
+    
+        card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`
+    
+    
+     } })
+    card.addEventListener('mouseleave', () => {
+        if(card.classList.contains('active')){ 
+        card.style.transform = `rotateX(0) rotateY(0)`
+}}) 
+    
+    card.addEventListener('click', () => {
+        cards.forEach(c => c.classList.remove('active'));
+        card.classList.add('active');
+        
+})
+});
